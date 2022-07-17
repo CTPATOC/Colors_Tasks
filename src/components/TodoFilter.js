6cf9fb4325604d18fdfc7d2a8bc31a550fdc53ff
Persei <johnsmith18@mailfence.com>
2022-07-17 19:01:38 +0300

    filter work

--- src/components/TodoFilter.js
+++ src/components/TodoFilter.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './TodoFilter.css';
 
-const TodoFilter = (props, {status, statusChangeHandler}) => {
+const TodoFilter = (props) => {
 
     return (
         <div className='todos-filter'>
             <div className='todos-filter__control'>
                 <label>Вибір за готовністю</label>
-                <select value={status} onChange={statusChangeHandler}>
+                <select value={props.status.toString()} onChange={props.onChangeStatus}>
                     <option value='all'>Усі завдання</option>
-                    <option value={(true)}>Виконані</option>
-                    <option value={(false)}>Активні</option>
+                    <option value={true}>Виконані</option>
+                    <option value={false}>Активні</option>
                 </select>
             </div>
         </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,15 +5,16 @@ import TodoFilter from './TodoFilter';
 
 const TodoList = (props) => {
     const [todos, setTodos] = useState([]);
-    const [filteredStatus, setFilteredStatus] = useState(false);
+    const [status, setStatus] = useState('all');
     
     useEffect(() => {
         const temp = localStorage.getItem('todos')
+        console.log(temp);
         const loadedTodos = JSON.parse(temp)
-
         if (loadedTodos)
         {
             setTodos(loadedTodos)
+            console.log(loadedTodos);
         }
     }, [])
 
@@ -22,16 +23,13 @@ const TodoList = (props) => {
         localStorage.setItem('todos', temp)
     }, [todos])
 
-    const statusChangeHandler = (status) => {
-        if (status === 'all')
-        {
-            setFilteredStatus(todos);
-        } else
-        {
-            let newTodos = [...todos].filter(todo => todo.status === status);
-            setFilteredStatus(newTodos);
-        };
+    const onChangeStatus = (event) => {
+        setStatus(event.target.value === 'all' ? 'all' : event.target.value === 'true');
     };
+    console.log(todos);
+    console.log(status);
+
+    const filteredTodo = status == 'all' ? todos : todos.filter(todo => todo.status === status)
 
     const addTodo = (todo) => {
         if (!todo.text || /^\s*$/.test(todo.text))
@@ -75,8 +73,8 @@ const TodoList = (props) => {
         <div>
             <h1>Які пригоди на сьогодні?</h1>
             <TodoForm onSubmit={addTodo} />
-            <TodoFilter status={filteredStatus} onChangeStatus={statusChangeHandler} />
-            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}
+            <TodoFilter status={status} onChangeStatus={onChangeStatus} />
+            <Todo todos={filteredTodo} completeTodo={completeTodo} removeTodo={removeTodo}
                 updateTodo={updateTodo} />
         </div>
     );
