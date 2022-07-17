302c7b4ee1d5755633eac81442b3b4e837ba58e5
Persei <johnsmith18@mailfence.com>
2022-07-17 17:14:31 +0300

    szfc

--- src/components/TodoFilter.js
+++ src/components/TodoFilter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './TodoFilter.css';
 
-const TodoFilter = (props, {statusChangeHandler}) => {
+const TodoFilter = (props, {status, statusChangeHandler}) => {
 
     return (
         <div className='todos-filter'>
             <div className='todos-filter__control'>
                 <label>Вибір за готовністю</label>
-                <select value={props.filteredStatus} onChange={statusChangeHandler}>
+                <select value={status} onChange={statusChangeHandler}>
                     <option value='all'>Усі завдання</option>
                     <option value={(true)}>Виконані</option>
                     <option value={(false)}>Активні</option>
